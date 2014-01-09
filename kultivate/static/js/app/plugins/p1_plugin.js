169bfc8a58156ec536d62b7c5a8cd7f5ac159b10
Ernst Naezer <ernstnaezer@gmail.com>
2014-01-09 17:30:10 +0100

    restored the gauge meter

--- kultivate/static/js/app/plugins/p1_plugin.js
+++ kultivate/static/js/app/plugins/p1_plugin.js
@@ -112,6 +112,123 @@ define(function (require) {
 		});
 	};
 
+	var createGaugeChart = function(){
+
+		$('#'+ options.prefix +'_gauge_chart').highcharts({
+		
+		    chart: {
+		        type: 'gauge',
+		        plotBackgroundColor: null,
+		        plotBackgroundImage: null,
+		        plotBorderWidth: 0,
+		        plotShadow: false
+		    },
+
+			credits: {
+				enabled:false
+			},	
+		    
+		    title: {
+		        text: 'actual power'
+		    },
+		    
+		    pane: {
+		        startAngle: -150,
+		        endAngle: 150,
+		        background: [{
+		            backgroundColor: {
+		                linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+		                stops: [
+		                    [0, '#FFF'],
+		                    [1, '#333']
+		                ]
+		            },
+		            borderWidth: 0,
+		            outerRadius: '109%'
+		        }, {
+		            backgroundColor: {
+		                linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+		                stops: [
+		                    [0, '#333'],
+		                    [1, '#FFF']
+		                ]
+		            },
+		            borderWidth: 1,
+		            outerRadius: '107%'
+		        }, {
+		            // default background
+		        }, {
+		            backgroundColor: '#DDD',
+		            borderWidth: 0,
+		            outerRadius: '105%',
+		            innerRadius: '103%'
+		        }]
+		    },
+		       
+		    // the value axis
+		    yAxis: {
+		        min: 0,
+		        max: 6000,
+		        
+		        minorTickInterval: 'auto',
+		        minorTickWidth: 1,
+		        minorTickLength: 10,
+		        minorTickPosition: 'inside',
+		        minorTickColor: '#666',
+		
+		        tickPixelInterval: 30,
+		        tickWidth: 2,
+		        tickPosition: 'inside',
+		        tickLength: 10,
+		        tickColor: '#666',
+		        labels: {
+		            step: 3,
+		            rotation: 'auto',
+					style: {
+						fontSize: '8px'
+					}
+		        },
+		        plotBands: [{
+		            from: 0,
+		            to: 2000,
+		            color: '#55BF3B' // green
+		        }, {
+		            from: 2000,
+		            to: 4000,
+		            color: '#DDDF0D' // yellow
+		        }, {
+		            from: 4000,
+		            to: 6000,
+		            color: '#DF5353' // red
+		        }]        
+		    },
+		
+		    series: [{
+		        name: 'W',
+		        data: [0],
+		        tooltip: {
+		            valueSuffix: ' W'
+		        },
+				dataLabels: {
+					enabled: true,
+				}
+		    }]
+		}, 
+		// Add some life
+		function (chart) {
+			if (!chart.renderer.forExport) {
+
+				var websocket = new WebSocket("ws://" + window.location.hostname + ":8081");
+				websocket.onmessage = function(evt) {
+					var data = JSON.parse(evt.data);
+					var point = chart.series[0].points[0];
+					point.update(parseInt(data.current_power_usage));
+				};
+			}
+		});
+	};
+
+
 	var updateChartWithNewRange = function(){
 
 	  	var range = rangeParser.parse($("#"+ options.prefix +"_date_range").val());
@@ -165,6 +282,7 @@ define(function (require) {
 				updateChartWithNewRange();
 			});
 
+			createGaugeChart();
 			updateDateRange(result);
 			createDailyChart(result);
 		}
