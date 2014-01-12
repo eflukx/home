b86855d9f65448a0d31eab7b197b89458f8f1f59
Ernst Naezer <ernstnaezer@gmail.com>
2014-01-12 21:31:49 +0100

    Trying some WebSocket live stuff

--- kultivate/static/js/app/plugins/p1_plugin.js
+++ kultivate/static/js/app/plugins/p1_plugin.js
@@ -113,121 +113,29 @@ define(function (require) {
 		});
 	};
 
-	var createGaugeChart = function(){
+	var last_measurements = [];
 
-		$('#'+ options.prefix +'_gauge_chart').highcharts({
-		
-		    chart: {
-		        type: 'gauge',
-		        plotBackgroundColor: null,
-		        plotBackgroundImage: null,
-		        plotBorderWidth: 0,
-		        plotShadow: false
-		    },
+	var installLiveListener = function(){
+		var websocket = new WebSocket("ws://" + window.location.hostname + ":8081");
+		websocket.onmessage = function(evt) {
+			var data = JSON.parse(evt.data);
+			$("current_power_consumption").text(data.value + "W");
 
-			credits: {
-				enabled:false
-			},	
-		    
-		    title: {
-		        text: 'actual power'
-		    },
-		    
-		    pane: {
-		        startAngle: -150,
-		        endAngle: 150,
-		        background: [{
-		            backgroundColor: {
-		                linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-		                stops: [
-		                    [0, '#FFF'],
-		                    [1, '#333']
-		                ]
-		            },
-		            borderWidth: 0,
-		            outerRadius: '109%'
-		        }, {
-		            backgroundColor: {
-		                linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-		                stops: [
-		                    [0, '#333'],
-		                    [1, '#FFF']
-		                ]
-		            },
-		            borderWidth: 1,
-		            outerRadius: '107%'
-		        }, {
-		            // default background
-		        }, {
-		            backgroundColor: '#DDD',
-		            borderWidth: 0,
-		            outerRadius: '105%',
-		            innerRadius: '103%'
-		        }]
-		    },
-		       
-		    // the value axis
-		    yAxis: {
-		        min: 0,
-		        max: 6000,
-		        
-		        minorTickInterval: 'auto',
-		        minorTickWidth: 1,
-		        minorTickLength: 10,
-		        minorTickPosition: 'inside',
-		        minorTickColor: '#666',
-		
-		        tickPixelInterval: 30,
-		        tickWidth: 2,
-		        tickPosition: 'inside',
-		        tickLength: 10,
-		        tickColor: '#666',
-		        labels: {
-		            step: 3,
-		            rotation: 'auto',
-					style: {
-						fontSize: '8px'
-					}
-		        },
-		        plotBands: [{
-		            from: 0,
-		            to: 2000,
-		            color: '#55BF3B' // green
-		        }, {
-		            from: 2000,
-		            to: 4000,
-		            color: '#DDDF0D' // yellow
-		        }, {
-		            from: 4000,
-		            to: 6000,
-		            color: '#DF5353' // red
-		        }]        
-		    },
-		
-		    series: [{
-		        name: 'W',
-		        data: [0],
-		        tooltip: {
-		            valueSuffix: ' W'
-		        },
-				dataLabels: {
-					enabled: true,
-				}
-		    }]
-		}, 
-		// Add some life
-		function (chart) {
-			if (!chart.renderer.forExport) {
+			last_measurements.push(data.value);
+			last_measurements = _.take(last_measurements, 10);
+			("#power_usage").sparkline(last_measurements, {
+				type: 'bar',
+				barColor: "#57889c",
+				height: "26px",
+//				barWidth: sparklineBarWidth,
+//				barSpacing: sparklineBarSpacing,
+//				stackedBarColor: sparklineStackedColor,
+//				negBarColor: sparklineNegBarColor,
+				zeroAxis: 'false'
+			});
 
-				var websocket = new WebSocket("ws://" + window.location.hostname + ":8081");
-				websocket.onmessage = function(evt) {
-					var data = JSON.parse(evt.data);
-					var point = chart.series[0].points[0];
-					point.update(parseInt(data.value));
-				};
-			}
-		});
-	};
+		};
+	}
 
 	var updateChartWithNewRange = function(){
 
